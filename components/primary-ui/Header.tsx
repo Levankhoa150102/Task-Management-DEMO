e8b03f55cd8c5ceed5d63bf5e08407cfaaf76287
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-25 17:59:16 +0700

    layout: header appbar

---
+++ components/primary-ui/Header.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { AppstoreFilled, BellOutlined, QuestionCircleOutlined, SearchOutlined, UserOutlined, } from '@ant-design/icons';
+import Badge from 'antd/es/badge';
+import Input from 'antd/es/input';
+import Avatar from 'antd/es/avatar'
+import Tooltip from 'antd/es/tooltip'
+function Header() {
+    return (
+        <div className='bg-[#1565c0] h-[var(--header-height)] flex items-center justify-between px-4'>
+            <div className='text-white flex items-center gap-2'>
+                <AppstoreFilled className='text-xl' />
+                <p className='text-xl font-bold '>Task management</p>
+            </div>
+
+            <div className=' flex items-center gap-4'>
+                <Input size="large" placeholder="Search..." prefix={<SearchOutlined />} />
+                <Tooltip title="Notification">
+                    <Badge count={9} size='small' className='cursor-pointer'>
+                        <BellOutlined className='text-2xl' color='white' />
+                    </Badge>
+                </Tooltip>
+                <Tooltip title="Help">
+                    <QuestionCircleOutlined className='text-2xl cursor-pointer'  />
+                </Tooltip>
+                <Tooltip title="Profile">
+                    <div className='w-16 h-16 flex items-center'>
+                        <Avatar icon={<UserOutlined />} className='cursor-pointer' />
+                    </div>
+                </Tooltip>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ pages/Board/BoardBar/BoardBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BoardBar() {
+    return (
+        <div>
+            Board Bar
+        </div>
+    );
+}
+
+export default BoardBar;
