d29e74f51a1b225d499c193b3338405844bb8025
khoalee <20521467@gm.uit.edu.vn>
2025-08-26 21:43:00 +0700

    sensor dnd on mobile

--- pages/Board/BoardContent/BoardContent.tsx
+++ pages/Board/BoardContent/BoardContent.tsx
@@ -2,11 +2,32 @@
 import { BoardType, ColumnType } from '@/types/Board';
 import ListColumns from './ListColumns/ListColumns';
 import { mapOrder } from '@/utils/Sort';
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { DndContext, DragEndEvent, PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core';
 import { useEffect, useState } from 'react';
 import { arrayMove } from '@dnd-kit/sortable';
 
 function BoardContent({ board }: { board: BoardType }) {
+    const pointerSensor = useSensor(PointerSensor, {
+        activationConstraint: { //Require a minimum distance of 10px to activate drag
+            distance: 10,
+        },
+    })
+    
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: { //Require a minimum distance of 10px to activate drag
+            distance: 10,
+        },
+    })
+
+    const touchSensor = useSensor(TouchSensor, {
+        activationConstraint:{
+            delay: 250,
+            tolerance: 5,
+        }
+    })
+
+    //const sensors = useSensors(pointerSenor)
+    const sensors = useSensors(mouseSensor, touchSensor) //Trãi nghiệm tốt trên mobile
     const [orderColumn, setOrderColumn] = useState<ColumnType[]>([])
     
     useEffect(() => {
@@ -30,7 +51,7 @@ function BoardContent({ board }: { board: BoardType }) {
 
     }
     return (
-        <DndContext onDragEnd={handleDragEnd}>
+        <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
             <div className='h-[var(--boardcontent-height)]  bg-[#1976D2] p-4'>
                 {/*Column Lists */}
                 <ListColumns columns={orderColumn} />
