614994969e7228be2880caa80fdc3c7a4b413c5e
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-26 17:48:01 +0700

    feat: drop drag basic fn

--- pages/Board/BoardContent/ListColumns/ListColumns.tsx
+++ pages/Board/BoardContent/ListColumns/ListColumns.tsx
@@ -1,22 +1,25 @@
+'use client'
 import { FileAddOutlined } from '@ant-design/icons';
 import Button from 'antd/es/button';
 import Column from './Column/Column';
 import { ColumnType } from '@/types/Board';
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable';
 
-
-function ListColumns({columns}: {columns: ColumnType[]}) {
+function ListColumns({ columns }: { columns: ColumnType[] }) {
 
     return (
-        <div className='flex gap-4 overflow-x-auto overflow-y-hidden my-[5px] py-[5px] h-[calc(var(--boardcontent-height)-24px)]'>
-            {columns?.map(column => <Column key={column._id} column={column} />)}
+        <SortableContext items={columns.map(column => column._id)} strategy={horizontalListSortingStrategy}>
+            <div className='flex gap-4 overflow-x-auto overflow-y-hidden my-[5px] py-[5px] h-[calc(var(--boardcontent-height)-24px)]'>
+                {columns?.map(column => <Column key={column._id} column={column} />)}
 
-             <div className='min-w-[200px] max-w-[300px]  mx-2 rounded-xs h-fit'>
-                <Button variant='outlined' className=' w-full' style={{ backgroundColor: 'transparent', color: 'white' }}>
-                    <FileAddOutlined  className='text-xl' />
-                    <span>Add new column</span>
-                </Button>
-            </div>  
-        </div> 
+                <div className='min-w-[200px] max-w-[300px]  mx-2 rounded-xs h-fit'>
+                    <Button variant='outlined' className=' w-full' style={{ backgroundColor: 'transparent', color: 'white' }}>
+                        <FileAddOutlined className='text-xl' />
+                        <span>Add new column</span>
+                    </Button>
+                </div>
+            </div>
+        </SortableContext>
     );
 }
 export default ListColumns;
