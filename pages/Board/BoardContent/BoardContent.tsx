b580c22bc1c18e50f268e8700dcc93d3c9894ec9
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-27 14:59:23 +0700

    dnd overplay column card

--- pages/Board/BoardContent/BoardContent.tsx
+++ pages/Board/BoardContent/BoardContent.tsx
@@ -1,10 +1,18 @@
 'use client'
-import { BoardType, ColumnType } from '@/types/Board';
+import { BoardType, CardType, ColumnType } from '@/types/Board';
 import ListColumns from './ListColumns/ListColumns';
 import { mapOrder } from '@/utils/Sort';
-import { DndContext, DragEndEvent, PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core';
+import { DndContext, DragEndEvent, PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor, DragStartEvent, DragOverlay, defaultDropAnimationSideEffects } from '@dnd-kit/core';
 import { useEffect, useState } from 'react';
 import { arrayMove } from '@dnd-kit/sortable';
+
+import Column from './ListColumns/Column/Column';
+import Card from './ListColumns/Column/CardLists/Card/Card';
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+    COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
+    CARD: 'ACTIVE_DRAG_ITEM_TYPE_CARD',
+}
 
 function BoardContent({ board }: { board: BoardType }) {
     const pointerSensor = useSensor(PointerSensor, {
@@ -12,7 +20,7 @@ function BoardContent({ board }: { board: BoardType }) {
             distance: 10,
         },
     })
-    
+
     const mouseSensor = useSensor(MouseSensor, {
         activationConstraint: { //Require a minimum distance of 10px to activate drag
             distance: 10,
@@ -20,7 +28,7 @@ function BoardContent({ board }: { board: BoardType }) {
     })
 
     const touchSensor = useSensor(TouchSensor, {
-        activationConstraint:{
+        activationConstraint: {
             delay: 250,
             tolerance: 5,
         }
@@ -29,17 +37,33 @@ function BoardContent({ board }: { board: BoardType }) {
     //const sensors = useSensors(pointerSenor)
     const sensors = useSensors(mouseSensor, touchSensor) //Trãi nghiệm tốt trên mobile
     const [orderColumn, setOrderColumn] = useState<ColumnType[]>([])
-    
+
+    //Confirm Card or Column is being dragged (for overplay)
+    const [activeDragItemId, setActiveDragItemId] = useState<string | null>(null)
+    const [activeDragItemType, setActiveDragItemType] = useState<string | null>(null)
+    const [activeDragItemData, setActiveDragItemData] = useState<ColumnType | CardType | null>(null)
+
     useEffect(() => {
         const orderedColumn = mapOrder(board.columns, board.columnOrderIds, '_id')
         setOrderColumn(orderedColumn)
     }, [board])
 
-    const handleDragEnd = (e: DragEndEvent ) => {
-        const {active, over} = e;
-        if(!over) return
-        if(active?.id !== over.id)
-        {
+    const handleDragStart = (e: DragStartEvent) => {
+        console.log('handleDragStart', e);
+        setActiveDragItemId(e?.active?.id as string)
+        setActiveDragItemType(e?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
+        setActiveDragItemData(e?.active?.data?.current as ColumnType | CardType | null)
+    }
+
+    console.log('activeDragItemId', activeDragItemId);
+    console.log('activeDragItemType', activeDragItemType);
+    console.log('activeDragItemData', activeDragItemData);
+
+    const handleDragEnd = (e: DragEndEvent) => {
+        console.log('handleDragEnd', e);
+        const { active, over } = e;
+        if (!over) return
+        if (active?.id !== over.id) {
             const oldIndex = orderColumn.findIndex((c: ColumnType) => c._id === active.id)
             const newIndex = orderColumn.findIndex((c: ColumnType) => c._id === over.id)
 
@@ -49,12 +73,35 @@ function BoardContent({ board }: { board: BoardType }) {
             setOrderColumn(dndOrderedColumns)
         }
 
+        setActiveDragItemId(null)
+        setActiveDragItemType(null)
+        setActiveDragItemData(null)
+
     }
+
+    const dropAnimation = {
+        sideEffects: defaultDropAnimationSideEffects({
+            styles: { active: { opacity: '0.5' } }
+        })
+    }
+
     return (
-        <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+        <DndContext
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            sensors={sensors}>
             <div className='h-[var(--boardcontent-height)]  bg-[#1976D2] p-4'>
                 {/*Column Lists */}
                 <ListColumns columns={orderColumn} />
+                <DragOverlay dropAnimation={dropAnimation}>
+                    {(!activeDragItemType) && null}
+                    {( activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) && (
+                        <Column column={activeDragItemData as ColumnType} />
+                    )}
+                    {( activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) && (
+                        <Card card={activeDragItemData as CardType} />
+                    )}
+                </DragOverlay>
             </div>
         </DndContext>
     );
--- pages/Board/BoardContent/ListColumns/Column/Column.tsx
+++ pages/Board/BoardContent/ListColumns/Column/Column.tsx
@@ -9,7 +9,7 @@ import CardLists from './CardLists/CardList';
 import { ColumnType } from '@/types/Board';
 import { mapOrder } from '@/utils/Sort';
 import { useSortable } from '@dnd-kit/sortable';
-import {CSS} from '@dnd-kit/utilities'
+import { CSS } from '@dnd-kit/utilities'
 
 const items: MenuProps['items'] = [
     {
@@ -72,52 +72,53 @@ const items: MenuProps['items'] = [
 ];
 function Column({ column }: { column: ColumnType }) {
     const [open, setOpen] = useState(false);
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
         id: column._id, data: { ...column }
     });
-    const dndKitColumnStyles = { transform: CSS.Translate.toString(transform), transition }
+    const dndKitColumnStyles = { transform: CSS.Translate.toString(transform), transition, height: '100%', opacity: isDragging ? 0.5 : 1 }
 
     const orderedCard = mapOrder(column.cards, column.cardOrderIds, '_id')
     return (
-        <div className='max-w-[300px] min-w-[300px] h-fit bg-[#EBECF0] rounded-md box-shadow-md max-h-[calc(var(--boardcontent-height)-50px)] overflow-y-auto'
-             ref={setNodeRef}
-             style={dndKitColumnStyles}
-             {...attributes}
-             {...listeners}
-
+        <div ref={setNodeRef}
+            style={dndKitColumnStyles}
+            {...attributes}
         >
-            {/*Column Header */}
-            <div className='p-4 flex justify-between items-center'>
-                <h2 className='text-lg font-semibold'>{column.title}</h2>
-                <Dropdown
-                    menu={{
-                        items,
-                        onClick: ({ key }) => {
-                            console.log('Dropdown selected key:', key);
-                        }
-                    }}
-                    trigger={['click']}
-                    onOpenChange={setOpen}
-                >
-                    <Space>
-                        <CaretDownOutlined
-                            className='cursor-pointer hover:scale-110 transition-transform duration-200'
-                            style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)' }}
-                        />
-                    </Space>
-                </Dropdown>
-            </div>
+            <div className='max-w-[300px] min-w-[300px] h-fit bg-[#EBECF0] rounded-md box-shadow-md max-h-[calc(var(--boardcontent-height)-50px)] overflow-y-auto'
+                {...listeners}
+            >
+                {/*Column Header */}
+                <div className='p-4 flex justify-between items-center'>
+                    <h2 className='text-lg font-semibold'>{column.title}</h2>
+                    <Dropdown
+                        menu={{
+                            items,
+                            onClick: ({ key }) => {
+                                console.log('Dropdown selected key:', key);
+                            }
+                        }}
+                        trigger={['click']}
+                        onOpenChange={setOpen}
+                    >
+                        <Space>
+                            <CaretDownOutlined
+                                className='cursor-pointer hover:scale-110 transition-transform duration-200'
+                                style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                            />
+                        </Space>
+                    </Dropdown>
+                </div>
 
-            {/*Column Cards List */}
-            <CardLists cards={orderedCard} />
+                {/*Column Cards List */}
+                <CardLists cards={orderedCard} />
 
-            {/*Column Footer */}
-            <div className='flex items-center justify-between p-4 '>
-                <Button color="default" variant="text">
-                    <FileAddOutlined style={{ color: '#1976D2' }} />
-                    <span className='text-[#1976D2] font-semibold'>Add new card</span>
-                </Button>
-                <MenuOutlined className='cursor-grab' />
+                {/*Column Footer */}
+                <div className='flex items-center justify-between p-4 '>
+                    <Button color="default" variant="text">
+                        <FileAddOutlined style={{ color: '#1976D2' }} />
+                        <span className='text-[#1976D2] font-semibold'>Add new card</span>
+                    </Button>
+                    <MenuOutlined className='cursor-grab' />
+                </div>
             </div>
         </div>
 
